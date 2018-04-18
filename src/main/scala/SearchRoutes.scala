586865ce7153315a6fc9d920086db5e41d054b76
FrancescoFucile <30294241+FrancescoFucile@users.noreply.github.com>
2018-04-18 23:36:34 +0200

    removed Utils. repsonse headers added via respondWithHeaders.

--- src/main/scala/SearchRoutes.scala
+++ src/main/scala/SearchRoutes.scala
@@ -1,4 +1,6 @@
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.{Directives, PathMatcher, Route}
+
 import scala.util._
 
 
@@ -9,18 +11,20 @@ trait SearchRoutes extends Directives with DatabaseInterface with PageRequestWeb
   lazy val searchRoutes =
     path("search") {
       get {
+        respondWithHeaders(RawHeader("Access-Control-Allow-Origin","*")) {
           parameters('subject.as[String], 'teacher.as[String]) {
             (subject, teacher) =>
               //val searchQuery_ID = (Math.random()*100000).toInt.toString
               val searchQuery_ID = 123456789.toString
               val AMQPMessage = s"{subject:$subject,teacher:$teacher,queryID:$searchQuery_ID}"
-              println("sending amqp message to db: "+ AMQPMessage)
+              println("sending amqp message to db: " + AMQPMessage)
               onComplete(searchNotes(AMQPMessage, searchQuery_ID)) {
-                  case Success(noteList) => complete(Utils.makeHttpResponse(noteList))
-                  //case Success(noteList) => complete("this is a response")
-                  case Failure(ex) => complete(s"error: $ex")
+                //case Success(noteList) => complete(Utils.makeHttpResponse(noteList))
+                case Success(noteList) => complete(noteList)
+                case Failure(ex) => complete(s"error: $ex")
               }
           }
+        }
       }
     }
 }
